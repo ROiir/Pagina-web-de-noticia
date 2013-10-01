e7fdd5705ab46ab2edc42488fb7e84ea1edb905b
ROiir <rodrigoramirezsoto@hotmail.com>
2013-09-30 22:36:14 -0500

    Modifique
    Cree las clases de la pagina de noticias

---
+++ Pagina-web-de-noticia/src/java/org/news/beans/Noticia.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.news.beans;
+
+/**
+ *
+ * @author ROii
+ */
+public class Noticia {
+    
+}
---
+++ Pagina-web-de-noticia/src/java/org/news/dao/CategoriaDao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.news.dao;
+
+/**
+ *
+ * @author ROii
+ */
+public class CategoriaDao {
+     public void EntrarAplicacion(){
+    }
+    public void RecuperarContrasena(){
+    }
+    public void CerrarSecion(){
+    }
+    public void ModificarPerfil(){
+    }
+    public void ConsultarNoticia(){
+    }
+} 
+
